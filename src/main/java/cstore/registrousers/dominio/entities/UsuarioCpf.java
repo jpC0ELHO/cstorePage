decb2d1fbc6f5dd9205f247663ca84c3f8cf11de
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-05 16:51:46 -0300

    update UsuarioCpf.java

---
+++ src/main/java/cstore/registrousers/dominio/entities/UsuarioCpf.java
@@ -0,0 +1,64 @@
+package cstore.registrousers.dominio.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import cstore.registrousers.dominio.entities.enums.Sexo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_usuarioscpf")
+@EntityListeners(AuditingEntityListener.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UsuarioCpf extends Entidade {
+
+    @CPF
+    @Column(unique = true, length = 15)
+    private String cpf;
+
+    @Column(nullable = false,unique = true,length = 10)
+    private String rg;
+
+    @Column(nullable = false)
+    private String primeiroNome;
+
+    @Column(nullable = false)
+    private String sobrenome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = false, length = 20)
+    private Sexo sexo;
+
+    @ElementCollection
+    @CollectionTable(name = "tb_usuariocpf_email",
+            joinColumns = @JoinColumn(name = "usuario_cpf", referencedColumnName = "cpf"))
+    @Column(name = "emails", nullable = false)
+    private Set<String> email;
+
+    @Column(nullable = false)
+    @JoinColumn(name = "usuario_cpf", referencedColumnName = "cpf")
+    @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
+    private Set<Telefone> telefone;
+
+    @Column(nullable = false)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
+
+    @Embedded
+    private Endereco endereco;
+}
