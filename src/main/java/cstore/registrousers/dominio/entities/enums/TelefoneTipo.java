1f9d3d56dd1a7fa96d8de6a1bb09521c93d462f2
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-05 16:56:03 -0300

    update TelefoneTipo.java

--- src/main/java/cstore/registrousers/dominio/entities/enums/TelefoneTipo.java
+++ src/main/java/cstore/registrousers/dominio/entities/enums/TelefoneTipo.java
@@ -1,4 +1,16 @@
 package cstore.registrousers.dominio.entities.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum TelefoneTipo {
+    PESSOAL("PESSOAL"),
+    RESIDENCIAL("RESIDENCIAL"),
+    OUTROS("OUTROS");
+
+    private String name;
+
+    TelefoneTipo(String name){
+        this.name=name;
+    }
 }
