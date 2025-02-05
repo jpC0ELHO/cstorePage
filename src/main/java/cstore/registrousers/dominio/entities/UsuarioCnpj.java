581229a80bbac82ce11cbe7000605f77d09dc7a9
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-05 16:50:52 -0300

    created UsuarioCnpj.java

--- src/main/java/cstore/registrousers/dominio/entities/UsuarioCnpj.java
+++ src/main/java/cstore/registrousers/dominio/entities/UsuarioCnpj.java
@@ -1,4 +1,23 @@
 package cstore.registrousers.dominio.entities;
 
-public class UsuarioCnpj {
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_usuariocnpj")
+@EntityListeners(AuditingEntityListener.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UsuarioCnpj extends Entidade{
 }
